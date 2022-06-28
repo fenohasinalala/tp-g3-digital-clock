493439c5ebfe2f0148974e193a7c1fbcdc6cd751
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2022-06-28 13:18:23 +0300

    Feat - Timer fonctionnele avec boutons start reset clock

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ import { useState } from "react";
 
 function App() {
   let [activeTimer, setActiveTimer] = useState(false);
+  let [startTimer, setStartTimer] = useState(false);
+  let [startReset, setStartReset] = useState(false);
   function changer() {
     activeTimer?setActiveTimer(false):setActiveTimer(true);
+    setStartTimer(false);
   }
-
+  function runTimer() {
+    startTimer?setStartTimer(false):setStartTimer(true);
+    setStartReset(false);
+  }
+  function runReset() {
+    setStartTimer(false)
+    setStartReset(true);
+  }
   return <>      
   {
-    activeTimer ? <Timer></Timer> : <Clock></Clock>
+    activeTimer ? <Timer boolTimer = {startTimer} boolReset = {startReset}/> : <Clock/>
   }  
   {
-    activeTimer ? <div className="btn_menu"><ButtonBase defComponent="START" defClass="btn_start" defFunction={changer}/><ButtonBase defComponent="RESET" defClass="btn_reset" defFunction={changer}/><ButtonBase defComponent="CLOCK" defClass="btn_clock" defFunction={changer}/> </div> : <ButtonBase defComponent="TIMER" defClass="btn_timer" defFunction={changer} />
+    activeTimer ? <div className="btn_menu">{startTimer?<ButtonBase defComponent="STOP" defClass="btn_start" defFunction={runTimer}/>:<ButtonBase defComponent="START" defClass="btn_start" defFunction={runTimer}/>}<ButtonBase defComponent="RESET" defClass="btn_reset" defFunction={runReset}/><ButtonBase defComponent="CLOCK" defClass="btn_clock" defFunction={changer}/> </div> : <ButtonBase defComponent="TIMER" defClass="btn_timer" defFunction={changer} />
   }
   </>    
 }
--- src/Timer/index.js
+++ src/Timer/index.js
@@ -1,32 +1,57 @@
 import { useState, useEffect } from 'react';
 import './styles.css';
 import ButtonBase from "../Components/ButtonBase.js";
+import runTimer from "../App.js";
 
-export default function Clock() {
-  const [date, setDate] = useState(new Date());
-  let timer;
+export default function Timer({boolTimer,boolReset}) {
+    const [date, setDate] = useState(new Date());
+    let [debut, setTempsDebut] = useState({ heures: date.getHours(), minutes: date.getMinutes(), secondes: date.getSeconds() });
+    let [tempsRestant, setTempsRestant] = useState({ heures: date.getHours(), minutes: date.getMinutes(), secondes: date.getSeconds() });
+    let  [startTimer, setStartTimer] = useState({runTimer});    
+    let timer;
 
-  useEffect(() => {
-    timer = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
+    console.log("booltimer : "+boolTimer);
+    console.log("boolReset : "+boolReset);    
 
-
+    useEffect(() => {        
+        timer = setInterval(() => {
+            if (boolReset){
+                setTempsRestant(debut);
+            }
+            if(boolTimer==true){
+            if (tempsRestant.heures != 0 || tempsRestant.minutes != 0 || tempsRestant.secondes != 0) {
+                let reste = { heures: tempsRestant.heures, minutes: tempsRestant.minutes, secondes: tempsRestant.secondes-1 };
+                
+                if (reste.secondes == -1) {
+                    reste.secondes = 59;
+                    reste.minutes = reste.minutes-1;
+                }
+                if (reste.minutes == -1) {
+                    reste.minutes = 59;
+                    reste.heures = reste.heures-1;
+                }
+                setTempsRestant(reste);            
+            }
+            else {
+                clearInterval(timer)
+            }
+        }
+        }, 1000);
     return () => {
-      clearInterval(timer);
+        clearInterval(timer);
     };
-  });
+});
 
-  //<ButtonBase defComponent="Timer here" defClass="btn_clock_here" defFunction="" /> to return
-  
-  return (
-  <div className="clock-container">
-    
-  <p className="clock">
-    <span>{date.getHours().toString().padStart(2, '0')}</span>:
-    <span>{date.getMinutes().toString().padStart(2, '0')}</span>:
-    <span>{date.getSeconds().toString().padStart(2, '0')}</span>
-  </p>
-  </div>
-  )
+
+//<ButtonBase defComponent="Timer here" defClass="btn_clock_here" defFunction="" /> to return
+
+return (
+    <div className="clock-container">
+        <p className="clock">
+            <span>{tempsRestant.heures.toString().padStart(2, '0')}</span>:
+            <span>{tempsRestant.minutes.toString().padStart(2, '0')}</span>:
+            <span>{tempsRestant.secondes.toString().padStart(2, '0')}</span>
+        </p>
+    </div>
+)
 }
