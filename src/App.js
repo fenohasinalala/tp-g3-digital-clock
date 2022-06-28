26cfecb11f7e20ddf8e6f68345e8da32b6118f0f
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2022-06-28 20:37:06 +0300

    Feat - ajout input

--- src/App.js
+++ src/App.js
@@ -6,28 +6,40 @@ import { useState } from "react";
 
 function App() {
   let [activeTimer, setActiveTimer] = useState(false);
+  let [debutTimer, setADebutTimer] = useState(false);
   let [startTimer, setStartTimer] = useState(false);
-  let [startReset, setStartReset] = useState(false);
+  let [startReset, setStartReset] = useState(true);
+
+  function changer0() {
+    activeTimer ? setActiveTimer(false) : setActiveTimer(true);
+    setStartTimer(false);
+    setADebutTimer(true);
+  }
   function changer() {
-    activeTimer?setActiveTimer(false):setActiveTimer(true);
+    activeTimer ? setActiveTimer(false) : setActiveTimer(true);
     setStartTimer(false);
   }
   function runTimer() {
-    startTimer?setStartTimer(false):setStartTimer(true);
+    startTimer ? setStartTimer(false) : setStartTimer(true);
     setStartReset(false);
+    setADebutTimer(false);
   }
   function runReset() {
-    setStartTimer(false)
+    setStartTimer(false);
     setStartReset(true);
+    setADebutTimer(true);
   }
-  return <>      
-  {
-    activeTimer ? <Timer boolTimer = {startTimer} boolReset = {startReset}/> : <Clock/>
-  }  
-  {
-    activeTimer ? <div className="btn_menu">{startTimer?<ButtonBase defComponent="STOP" defClass="btn_start" defFunction={runTimer}/>:<ButtonBase defComponent="START" defClass="btn_start" defFunction={runTimer}/>}<ButtonBase defComponent="RESET" defClass="btn_reset" defFunction={runReset}/><ButtonBase defComponent="CLOCK" defClass="btn_clock" defFunction={changer}/> </div> : <ButtonBase defComponent="TIMER" defClass="btn_timer" defFunction={changer} />
-  }
-  </>    
+
+  const btn_start_resume = debutTimer ? <ButtonBase defComponent="START" defClass="btn_start" defFunction={runTimer} /> : <ButtonBase defComponent="RESUME" defClass="btn_start" defFunction={runTimer} />
+
+  return <>
+    {
+      activeTimer ? <Timer boolTimer={startTimer} boolReset={startReset} boolDebut ={debutTimer} /> : <Clock />
+    }
+    {
+      activeTimer ? <div className="btn_menu">{startTimer ? <ButtonBase defComponent="PAUSE" defClass="btn_start" defFunction={runTimer} /> : btn_start_resume}<ButtonBase defComponent="RESET" defClass="btn_reset" defFunction={runReset} /><ButtonBase defComponent="CLOCK" defClass="btn_clock" defFunction={changer} /> </div> : <ButtonBase defComponent="TIMER" defClass="btn_timer" defFunction={changer0} />
+    }
+  </>
 }
 
 export default App;
--- src/Timer/index.js
+++ src/Timer/index.js
@@ -1,27 +1,37 @@
 import { useState, useEffect } from 'react';
 import './styles.css';
-import ButtonBase from "../Components/ButtonBase.js";
-import runTimer from "../App.js";
+import { useFormik } from 'formik';
 
-export default function Timer({boolTimer,boolReset}) {
+export default function Timer({boolTimer,boolReset,boolDebut}) {
     const [date, setDate] = useState(new Date());
     let [debut, setTempsDebut] = useState({ heures: date.getHours(), minutes: date.getMinutes(), secondes: date.getSeconds() });
-    let [tempsRestant, setTempsRestant] = useState({ heures: date.getHours(), minutes: date.getMinutes(), secondes: date.getSeconds() });
-    let  [startTimer, setStartTimer] = useState({runTimer});    
-    let timer;
-
-    console.log("booltimer : "+boolTimer);
-    console.log("boolReset : "+boolReset);    
+    
+    
+    const formik = useFormik({
+        initialValues:{
+            heures: debut.heures,
+            minutes: debut.minutes,
+            secondes: debut.secondes
+        }
+    });
+    let [tempsRestant, setTempsRestant] = useState(debut);
+    let [temps, setTemps] = useState(debut);
+    
+    let timer;      
+    
+    
+    console.log("temps restant : " + tempsRestant);
 
     useEffect(() => {        
         timer = setInterval(() => {
+                        
             if (boolReset){
-                setTempsRestant(debut);
+                setTempsRestant(formik.values);
             }
             if(boolTimer==true){
             if (tempsRestant.heures != 0 || tempsRestant.minutes != 0 || tempsRestant.secondes != 0) {
                 let reste = { heures: tempsRestant.heures, minutes: tempsRestant.minutes, secondes: tempsRestant.secondes-1 };
-                
+                console.log("reste"+reste);
                 if (reste.secondes == -1) {
                     reste.secondes = 59;
                     reste.minutes = reste.minutes-1;
@@ -33,6 +43,7 @@ export default function Timer({boolTimer,boolReset}) {
                 setTempsRestant(reste);            
             }
             else {
+                alert("TIME IS UP");
                 clearInterval(timer)
             }
         }
@@ -43,15 +54,26 @@ export default function Timer({boolTimer,boolReset}) {
 });
 
 
+console.log(formik.values);
 //<ButtonBase defComponent="Timer here" defClass="btn_clock_here" defFunction="" /> to return
 
 return (
+    boolDebut?
     <div className="clock-container">
-        <p className="clock">
-            <span>{tempsRestant.heures.toString().padStart(2, '0')}</span>:
-            <span>{tempsRestant.minutes.toString().padStart(2, '0')}</span>:
-            <span>{tempsRestant.secondes.toString().padStart(2, '0')}</span>
-        </p>
+    <p className="clock">
+    <input id="heures" type="number" value={formik.values.heures} onChange={formik.handleChange} className="input_time" min="0" max="23"/>:
+    <input id="minutes" type="number" value={formik.values.minutes} onChange={formik.handleChange} className="input_time"  min="0" max="59"/>:
+    <input id="secondes" type="number" value={formik.values.secondes} onChange={formik.handleChange} className="input_time" min="0" max="59"/>
+    </p>
     </div>
+    : 
+    <div className="clock-container">
+    <p className="clock">
+    <span>{tempsRestant.heures.toString().padStart(2, '0')}</span>:
+    <span>{tempsRestant.minutes.toString().padStart(2, '0')}</span>:
+    <span>{tempsRestant.secondes.toString().padStart(2, '0')}</span>
+    </p>
+    </div>
+
 )
 }
